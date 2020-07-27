f500ad05cf507132e7fc94bcaa35d20d14dc851f
byeongcheoljo <max91128@naver.com>
2020-07-27 14:05:06 +0900

    Update bert_ner.py

--- bert_ner.py
+++ bert_ner.py
@@ -48,7 +48,6 @@ print(torch.cuda.get_device_name(0))
 
 max_length = 100
 batch_size = 32
-
 tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=False)
 
 def tokenize_and_preserve_labels(sentence, text_labels):
@@ -78,3 +77,39 @@ labels = [token_label_pair[1] for token_label_pair in tokenized_texts_and_labels
 
 print(tokenized_texts[1])
 print(labels[1])
+
+
+### data : sentence 문장 길이 padding으로 맞춰주기 
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+                          maxlen=max_length, dtype="long", value=0.0,
+                          truncating="post", padding="post")
+
+tags = pad_sequences([[tag2idx.get(l) for l in lab] for lab in labels],
+                     maxlen=max_length, value=tag2idx["PAD"], padding="post",
+                     dtype="long", truncating="post")
+
+##문장에 있는 값이 padding 이면 0, 아니면 1
+attention_masks = [[float(i != 0.0) for i in x] for x in input_ids]
+
+## train data와 valid data split하기
+tr_inputs, val_inputs, tr_tags, val_tags = train_test_split(input_ids, tags,
+                                                            random_state=2018, test_size=0.1)
+tr_masks, val_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                             random_state=2018, test_size=0.1)
+
+## data를 Torch Tensor로 변환
+tr_inputs = torch.tensor(tr_inputs)
+val_inputs = torch.tensor(val_inputs)
+tr_tags = torch.tensor(tr_tags)
+val_tags = torch.tensor(val_tags)
+tr_masks = torch.tensor(tr_masks)
+val_masks = torch.tensor(val_masks)
+
+# 파이토치의 DataLoader로 입력, 마스크, 라벨을 묶어 데이터 설정
+train_data = TensorDataset(tr_inputs, tr_masks, tr_tags)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+valid_data = TensorDataset(val_inputs, val_masks, val_tags)
+valid_sampler = SequentialSampler(valid_data)
+valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=batch_size)
