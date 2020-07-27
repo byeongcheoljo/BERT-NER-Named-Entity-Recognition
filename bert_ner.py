5a11b6f1e110ae52fa8d062e97778f5498bcb9a7
byeongcheoljo <max91128@naver.com>
2020-07-27 10:47:29 +0900

    Update bert_ner.py

--- bert_ner.py
+++ bert_ner.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm, trange
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertConfig
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("nerDataSet/ner_dataset.csv", encoding="latin1").fillna(method="ffill")
 print(data.head(10))
-
 
 class SentenceGetter():
     def __init__(self, data):
@@ -21,6 +25,7 @@ class SentenceGetter():
             s = self.grouped("sentece:{}".format(self.n_sent))
             self.nsent +=1
             return s
+
         except:
             return None
 
@@ -34,3 +39,42 @@ print(labels[0])
 tag_values = list(set(data["Tag"].values))
 tag_values.append("PAD")
 tag2idx = {t: i for i, t in enumerate(tag_values)}
+print(tag2idx)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+print(torch.cuda.get_device_name(0))
+
+max_length = 100
+batch_size = 32
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=False)
+
+def tokenize_and_preserve_labels(sentence, text_labels):
+    tokenized_sentence = []
+    labels = []
+
+    for word, label in zip(sentence, text_labels):
+
+        tokenized_word = tokenizer.tokenize(word)
+        n_subwords = len(tokenized_word)
+
+        tokenized_sentence.extend(tokenized_word)
+
+        labels.extend([label] * n_subwords)
+
+    return tokenized_sentence, labels
+
+
+tokenized_texts_and_labels = [
+    tokenize_and_preserve_labels(sent, labs)
+    for sent, labs in zip(sentences, labels)
+]
+
+tokenized_texts = [token_label_pair[0] for token_label_pair in tokenized_texts_and_labels]
+labels = [token_label_pair[1] for token_label_pair in tokenized_texts_and_labels]
+
+
+print(tokenized_texts[1])
+print(labels[1])
