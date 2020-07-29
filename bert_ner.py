0ee103b2d5fea22e66dffebb31f2d0226f87cbda
byeongcheoljo <max91128@naver.com>
2020-07-29 09:47:59 +0900

    update BERT Code. (bert_ner.py)

--- bert_ner.py
+++ bert_ner.py
@@ -48,7 +48,7 @@ print(torch.cuda.get_device_name(0))
 
 max_length = 100
 batch_size = 32
-tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=False)
+tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased", do_lower_case = False)
 
 def tokenize_and_preserve_labels(sentence, text_labels):
     tokenized_sentence = []
@@ -113,3 +113,119 @@ train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batc
 valid_data = TensorDataset(val_inputs, val_masks, val_tags)
 valid_sampler = SequentialSampler(valid_data)
 valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=batch_size)
+
+
+model = BertForTokenClassification.from_pretrained("bert-base-multilingual-cased", num_labels = len(tag2idx), output_attentions = False, output_hidden_states = False)
+model.cuda()
+
+FULL_FINETUNING = True
+if FULL_FINETUNING:
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.0} ]
+else:
+    param_optimizer = list(model.classifier.named_parameters())
+    optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr = 3e-5, eps = 1e-8)
+
+epochs = 10
+max_grad_norm = 1.0
+total_steps = len(train_dataloader) * epochs
+
+# Create the learning rate scheduler.
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0, num_training_steps = total_steps)
+
+def format_time(elapsed):
+    # 반올림
+    elapsed_rounded = int(round((elapsed)))
+    # hh:mm:ss으로 형태 변경
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+loss_values, validation_loss_values = [], []
+for _ in trange(epochs, desc="Epoch"):
+    print("="*10,"training","="*10)
+    t0 = time.time()
+    model.train()
+    total_loss = 0
+
+    # Training loop
+    for step, batch in enumerate(train_dataloader):
+        if step % 500 == 0 and not step == 0:
+            elapsed = format_time(time.time() - t0)
+            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        model.zero_grad()
+        outputs = model(b_input_ids, token_type_ids=None,
+                        attention_mask = b_input_mask, labels = b_labels)
+        loss = outputs[0]
+        loss.backward()
+        total_loss += loss.item()
+        torch.nn.utils.clip_grad_norm_(parameters = model.parameters(), max_norm = max_grad_norm)
+        optimizer.step()
+        scheduler.step()
+
+    avg_train_loss = total_loss / len(train_dataloader)
+    print("Average train loss: {}".format(avg_train_loss))
+
+    loss_values.append(avg_train_loss)
+
+    print("="*10,"validation","="*10)
+    model.eval()
+
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+    predictions , true_labels = [], []
+    for batch in valid_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        ##gradient 계산 안함
+        with torch.no_grad():
+            outputs = model(b_input_ids, token_type_ids = None,
+                            attention_mask = b_input_mask, labels = b_labels)
+        logits = outputs[1].detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+
+        eval_loss += outputs[0].mean().item()
+        predictions.extend([list(p) for p in np.argmax(logits, axis = 2)])
+        true_labels.extend(label_ids)
+
+    eval_loss = eval_loss / len(valid_dataloader)
+    validation_loss_values.append(eval_loss)
+    print("Validation loss: {}".format(eval_loss))
+    pred_tags = [tag_values[p_i] for p, l in zip(predictions, true_labels)
+                                 for p_i, l_i in zip(p, l) if tag_values[l_i] != "PAD"]
+    valid_tags = [tag_values[l_i] for l in true_labels
+                                  for l_i in l if tag_values[l_i] != "PAD"]
+    print("Validation Accuracy: {}".format(accuracy_score(pred_tags, valid_tags)))
+    print("Validation F1-Score: {}".format(f1_score(pred_tags, valid_tags)))
+    print()
+
+## Test Set 테스트 해보기
+test_sentence = """
+Twitter put a 12-hour restriction on Donald Trump Jr.'s account,
+saying the president's son put out a tweet that contained
+"misleading and potentially harmful" information about the coronavirus.
+"""
+tokenized_sentence = tokenizer.encode(test_sentence)
+input_ids = torch.tensor([tokenized_sentence]).cuda()
+with torch.no_grad():
+    output = model(input_ids)
+label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
+# join bpe split tokens
+tokens = tokenizer.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
+new_tokens, new_labels = [], []
+for token, label_idx in zip(tokens, label_indices[0]):
+    if token.startswith("##"):
+        new_tokens[-1] = new_tokens[-1] + token[2:]
+    else:
+        new_labels.append(tag_values[label_idx])
+        new_tokens.append(token)
+for token, label in zip(new_tokens, new_labels):
+    print("{}\t{}".format(label, token))
